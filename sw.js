65a2e432914471f292c19788553a249dfcd7d72b
Pieces <piecesnbits@protonmail.com>
2025-11-07 19:49:40 +0100

    ddd

--- sw.js
+++ sw.js
@@ -1,45 +1,12 @@
 // sw.js
-const PWAServiceWorker = (() => {
-  const CACHE_NAME = 'app-cache-v1';
+self.addEventListener('install', event => {
+  console.log('Service Worker installing.');
+});
 
-  function onInstall(event) {
-    console.log('Service Worker installing.');
-    // Uncomment to force immediate activation
-    // self.skipWaiting();
-  }
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated.');
+});
 
-  function onActivate(event) {
-    console.log('Service Worker activated.');
-    event.waitUntil(
-      caches.keys().then(keys =>
-        Promise.all(
-          keys
-            .filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-        )
-      )
-    );
-    // Uncomment to claim control immediately after activation
-    // event.waitUntil(self.clients.claim());
-  }
-
-  async function fetchHandler(event) {
-    try {
-      return await fetch(event.request);
-    } catch {
-      return caches.match(event.request);
-    }
-  }
-
-  return {
-    install: onInstall,
-    activate: onActivate,
-    fetch: fetchHandler
-  };
-})();
-
-self.addEventListener('install', PWAServiceWorker.install);
-self.addEventListener('activate', PWAServiceWorker.activate);
 self.addEventListener('fetch', event => {
-  event.respondWith(PWAServiceWorker.fetch(event));
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
