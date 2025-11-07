9465147ac4874d68b42c88841fcdb343829beaca
Pieces <piecesnbits@protonmail.com>
2025-11-07 19:05:22 +0100

    Initial commit message

---
+++ sw.js
@@ -0,0 +1,12 @@
+// sw.js
+self.addEventListener('install', event => {
+  console.log('Service Worker installing.');
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated.');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
